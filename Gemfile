45b66511cc32ac7752c3502e3d83533a9007e88a
Robert Morris <obviouslyrob@gmail.com>
2015-09-02 17:41:22 +0000

    fixed an earlier authenticate issue in enrollments, testing lesson through entering CC info, everything seems to be working, continuing lesson

--- Gemfile
+++ Gemfile
@@ -40,6 +40,7 @@ gem 'figaro'
 gem 'fog', '~> 1.3.1'
 gem 'mini_magick'
 gem 'carrierwave-aws'
+gem 'stripe'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.1.2'
--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -1,4 +1,5 @@
 class EnrollmentsController < ApplicationController
+	before_action :authenticate_user!
 
   def create
   	current_user.enrollments.create(:course => current_course)
