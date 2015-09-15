4b3794102f839fd0b6764a358cbe748f6c8f6668
Robert Morris <obviouslyrob@gmail.com>
2015-09-15 16:06:18 +0000

    Moved the lesson create function into a modal and connected the create capability to a jQuery function

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,14 @@ Flixter2::Application.routes.draw do
   devise_for :users
   resource :dashboard, :only => [:show]
   root 'static_pages#index'
-  resources :courses, :only => [:index, :show]
   resources :courses, :only => [:index, :show] do
     resources :enrollments, :only => :create
   end
   resources :lessons, :only => [:show]
   namespace :instructor do
     resources :lessons, :only => [:update]
-    resources :sections, :only => [:update]
     resources :sections, :only => [:update] do
-      resources :lessons, :only => [:new, :create, :update]
+      resources :lessons, :only => [:create]
     end
     resources :courses, :only => [:new, :create, :show] do
       resources :sections, :only => [:create]
